f422425b15cda3197c9fe5ce9db50a7a85129639
Ricardo Mejia <ricky-97@live.com.mx>
2024-10-14 12:01:52 -0600

    Se modifico

--- src/main/java/com/escuela/EscuelaApplication.java
+++ src/main/java/com/escuela/EscuelaApplication.java
@@ -8,7 +8,7 @@ public class EscuelaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EscuelaApplication.class, args);
-		System.out.println("HOla");
+		System.out.println("Hola es un nuevo cambio");
 	}
 
 }
